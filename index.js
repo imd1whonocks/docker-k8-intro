5cd8020f6b94154310f840c2456ff1012d19ee0e
imd1whonocks <shashank.2410@gmail.com>
2021-12-19 13:43:17 +0530

    Basic app done

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+)
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public' ,'/index.html'));
+})
+app.get('/page1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public' ,'/page.html'));
+})
+app.post('/post', (req, res) => {
+    console.log('Parameter : ', req.body.field);
+    res.redirect('/');
+})
+
+app.get('/crash', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public' ,'/crashed.html'));
+    setTimeout(() => {
+        process.exit(1)
+    }, 2000)
+})
+
+app.listen(8080, () => {
+    console.log('Server running on 8080')
+})
+
